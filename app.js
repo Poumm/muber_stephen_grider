0f6bf3d2291de9f9fe2c214dd0a3bfa78e597bb9
jl <jl@axin.fr>
2017-11-10 14:30:36 +0100

    gestion erreur et mep test environnement

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ const mongoose = require("mongoose");
 const routes = require("./routes/routes");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/mumber", { useMongoClient: true });
+if (process.env.NODE_ENV !== "test")
+  mongoose.connect("mongodb://localhost/mumber", { useMongoClient: true });
 
-//Doivent être définies dans cet ordre
+// Doivent être définies dans cet ordre
+// En effet l'ordre défini ici est l'ordre dans lequel les middleware sont appelés
 app.use(bodyParser.json());
+
 routes(app);
+
+app.use((err, req, res, next) => {
+  res.status(422).send({ error: err.message });
+});
 
 module.exports = app;
---
+++ test/test_helper.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+before(done => {
+  process.env.NODE_ENV = "test";
+
+  mongoose.connect("mongodb://localhost/mumber_test", { useMongoClient: true });
+  mongoose.connection.once("open", () => done()).on("error", err => {
+    console.warn("Warning", err);
+  });
+});
+
+beforeEach(done => {
+  const { drivers } = mongoose.connection.collections;
+  drivers
+    .drop()
+    .then(() => done())
+    .catch(() => done());
+});
